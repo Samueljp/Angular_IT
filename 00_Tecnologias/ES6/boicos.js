39a862006847171af81c9c942a6137c83e0fd1e4
Samueljp <samueljpitis@gmail.com>
2017-10-24 17:22:23 +0200

    ES6

---
+++ 00_Tecnologias/ES6/boicos.js
@@ -0,0 +1,14 @@
+var data = [{ precio: 12 }, { precio: 34 }, { precio: 19 }];
+
+data.forEach(elem => elem.precio++);
+
+data.forEach(elem => {
+    if (true) {
+        var iva = 1.16
+        var precioFinal = elem.precio * iva
+        console.log(`
+    Oferta:
+    El precio final es ${precioFinal}`);
+    }
+     console.log (iva)
+});
